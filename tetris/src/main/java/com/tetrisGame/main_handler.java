75ae67ec346c3143e12b13ff230cf9d831341170
Petkovic <andrija.petkovic06@gmail.com>
2024-06-18 11:40:32 +0200

    geändert:       .vscode/settings.json 	geändert:       tetris/src/main/java/com/tetrisGame/main_handler.java

--- tetris/src/main/java/com/tetrisGame/main_handler.java
+++ tetris/src/main/java/com/tetrisGame/main_handler.java
@@ -1,18 +1,37 @@
-package com.tetris;
+package com.tetrisGame;
 
-public class main_handler {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
 
-    int[][] grid = new int[30][10];
+public class main_handler extends Application {
 
+    int[][] grid = new int[20][10];
 
     public void output() {
+        System.out.print("  _");
         for (int i = 0; i < grid.length; i++) {
-            
+            System.out.print("_");
         }
-    }
+        System.out.println("  ");
 
-    public void start() {
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print("<! ");
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.print(". ");
+            }
+            System.out.println("!>");
+        }
 
+        System.out.print("  ¯");
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print("¯");
+        }
+        System.out.println("¯");
     }
 
     public void update() {
@@ -20,7 +39,18 @@ public class main_handler {
     }
 
     public static void main(String[] arg) {
-        new main_handler().start();
+        launch(arg);
     }
 
+    @Override
+    public void start(Stage primaryStage) {
+
+        BorderPane pane = new BorderPane();
+
+        Scene scene = new Scene(pane, 800, 300);
+
+        primaryStage.setTitle("BorderPane");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
