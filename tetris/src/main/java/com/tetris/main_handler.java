eb34adadafd0d65efbac9101ce7a040161e76f3d
Petkovic <andrija.petkovic06@gmail.com>
2024-06-18 10:21:02 +0200

    geändert:       tetris/src/main/java/com/tetris/main_handler.java

--- tetris/src/main/java/com/tetris/main_handler.java
+++ tetris/src/main/java/com/tetris/main_handler.java
@@ -4,4 +4,20 @@ public class main_handler {
 
     int[][] grid = new int[10][30];
 
+    public void update() {
+
+    }
+
+    public void output() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public static void main(String[] arg) {
+        new main_handler().start();
+    }
+
 }
